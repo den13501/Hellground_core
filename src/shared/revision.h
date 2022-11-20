d897425cea1c3722443d160efe66b21c4f6e5bb1
-Shiken- <den13501@yahoo.com.tw>
2022-11-21 05:44:27 +0800

    Add components of compiling for x64 environment
    Just a test.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,5 +1,11 @@
-#ifndef __REVISION_DATA_H__
-#define __REVISION_DATA_H__
- #define REVISION_HASH                      "8b921a2c69e4da00a284"
- #define REVISION_DATE                      "2022-06-16 16:35:55 +0300"
-#endif // __REVISION_DATA_H__
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _REVISION "*"
+ #define _HASH "*"
+ #define _REVISION_DATE "*"
+ #define _REVISION_TIME "*"
+ #define FILEVER        0,0,0,0
+ #define PRODUCTVER     0,0,0,0
+ #define STRFILEVER     "0, 0, *, *"
+ #define STRPRODUCTVER  "0, 0, *, *"
+#endif // __REVISION_H__
